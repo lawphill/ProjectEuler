fbe03c1213ad9cab5b7a633cde2fc0a5fc821eba
Lawrence Phillips <lawphill@gmail.com>
2015-07-21 19:50:34 -0700

    Solved 75

---
+++ Problems_76_to_100/Euler076.R
@@ -0,0 +1,13 @@
+# Counting summations
+# 5 can be written as a sum in 6 unique ways:
+#   4+1
+#   3+2
+#   3+1+1
+#   2+2+1
+#   2+1+1+1
+#   1+1+1+1+1
+
+# Note, 1 has 0 ways, 2 has 1 way, 3 has 2 ways, 4 has 4 ways, 5 has 6 ways
+
+# How many ways can 100 be written as the sum of at least two positive integers?
+
